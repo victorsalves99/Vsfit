4e25bdb5577454d731f39283ef8415c34dafba5f
victorsalves99 <victorsalves99@outlook.com>
2023-04-06 20:47:59 -0300

    resolvido problema de rederisação da imagem da home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Home.css"
+import mulher from "../../img/mulher.png"
 
 const Home = () => {
   return (
     <section className='Home'>
         <div className='home'>
-            <img src="../../../../public/image/mulher.png" alt="" />
+            <img src={mulher} alt="" />
             <p>O exercício físico traz uma infinidade de benefícios para o corpo e para a mente. Pode melhorar a saúde cardiovascular, construir força e resistência muscular, reduzir o risco de doenças crônicas e promover o bem-estar mental. O exercício regular também pode melhorar a qualidade do sono, aumentar os níveis de energia e aumentar a produtividade geral e a longevidade.</p>
         </div>
     </section>
